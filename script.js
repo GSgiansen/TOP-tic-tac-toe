c3fcfce287104e8b7c439501ea000afba7913a99
GianSen <giansenrockz@gmail.com>
2022-07-18 17:40:28 +0800

    Included logic for checkingwhoever wins game

--- script.js
+++ script.js
@@ -2,12 +2,14 @@
 
 const GameBoard={
     gameboard:["","","","","","","","",""],
+    gameover:false,
 
 }
 
 function Players(name){
     name:name
 }
+
 
 function genCircle(){
     let circle=document.createElement("div")
@@ -23,26 +25,7 @@ cross.classList.add("cross")
 cross.textContent="X"
 return cross
 }
-function playerMove(event){
-    if (!event.target.id){//encountered a big where clicking in the middle of O registers the O and not the sqyare
-        console.log("clown")
-        return
-    }
-    index=event.target.id
-    let selectedSquare=document.getElementById(index)
 
-    if (selectedSquare.childNodes.length==0) 
-    {
-        c=genCircle();
-        selectedSquare.appendChild(c);
-        addToArray(index,"O")
-        AImove();
-        
-    }
-    
-    else console.log("clown")
-
-}
 
 let container=document.getElementById("container")
 let i=0
@@ -91,7 +74,35 @@ function addToArray(index,typeOfInput){//type of input can be cross or circle
 }
 
 //to work on is the AI logic to find a suitable move to put cross
+
+function playerMove(event){
+    if (!event.target.id){//encountered a bug where clicking in the middle of O registers the O and not the sqyare
+        console.log("clown")
+        return
+    }
+    index=event.target.id
+    let selectedSquare=document.getElementById(index)
+
+    if (selectedSquare.childNodes.length==0) 
+    {
+        c=genCircle();
+        selectedSquare.appendChild(c);
+        addToArray(index,"O")
+        check=checkWinner(getCircle())
+        if (check){
+            displayGameOver("player")
+            return
+        }
+        setTimeout(AImove(),1000);
+        
+    }
+    
+    else console.log("clown")
+
+}
+
 function AImove(){
+    setTimeout(function(){
     //scan array for empty 
     let emptySq=[]
     g=GameBoard.gameboard
@@ -107,10 +118,65 @@ function AImove(){
     }
     let randomindex=Math.floor(Math.random()*emptySq.length)
     addToArray(emptySq[randomindex],"X")
-    console.log(emptySq[randomindex])
 
 
     let selectedSquare=document.getElementById(emptySq[randomindex])
     selectedSquare.appendChild(genCross())
+    check=checkWinner(getCross())
+    if (check){
+        displayGameOver("AI")
+    }
+    
+},500)}
+
+function checkWinner(arr){
+    //diff between at least 3 squares must be constant
+    if (arr.length<3) return false;
+    let dict =new Object()
+    let numiterations=arr.length-2
+    for (let i=0;i<numiterations;i++){
+        console.log(arr[i],arr[i+1],arr[i+2])
+        if (Math.abs(arr[i]-arr[i+1])==Math.abs(arr[i+1]-arr[i+2])){
+            console.log("winner")
+            return true
+        }
+    }
+
+    return false
+
+}
+
+function getCross(){
+    let arr=[]
+    g=GameBoard.gameboard
+    for (index in g){
+        if (g[index]=="X"){
+            arr.push(index)
+        }
+    }
+    console.log(arr)
+    return arr
+
+}
+
+function getCircle(){
+    let arr=[]
+    g=GameBoard.gameboard
+    for (index in g){
+        if (g[index]=="O"){
+            arr.push(index)
+        }
+    }
+    console.log(arr)
+    return arr
+
+}
+
+function displayGameOver(winner){
+    GameBoard.gameover=true
+    let d= document.getElementById("display")
+    console.log(d)
+    d.textContent=winner+" has Won!"
+    d.style.display="flex"
     
 }
